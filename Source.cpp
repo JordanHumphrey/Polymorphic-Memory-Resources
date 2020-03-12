40b0668ae793183e161c40c19b564b339eefa197
Jordan Humphrey <jphumphrey2@gmail.com>
2020-03-11 23:20:27 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <unordered_map>
 #include <array>
 #include <memory_resource>
 #include <cstdlib> // for std::byte
@@ -218,7 +219,99 @@ void chainMemRes() {
 } // deallocates all allocated memory
 #pragma endregion
 
+#pragma region Null_Memory_Resource
+void exampleNMR() {
+	/*
+	Null_memory_resource handles allocations in a way that each 
+	allocation throws a bad_alloc exception. The most important
+	application is to ensure that a memory pool using memory
+	allocated on the stack does not suddenly allocate on the heap
+	if it needs more.
+	*/
+
+	// use memory on the stack without fallback on the heap:
+	std::array<std::byte, 200000> buf;
+	std::pmr::monotonic_buffer_resource pool{ buf.data(), buf.size(), std::pmr::null_memory_resource() };
+
+	// and allocate too much memory
+	std::pmr::unordered_map<long, std::pmr::string> coll{ &pool };
+	try {
+		for (int i = 0; i < buf.size(); ++i) {
+			std::string a{ "Customer" + std::to_string(i) };
+			coll.emplace(i, a);
+		}
+	}
+	catch (const std::bad_alloc & e) {
+		std::cerr << "BAD ALLOC EXCEPTION: " << e.what() << '\n';
+	}
+	std::cout << "size: " << coll.size() << '\n';
+}
+#pragma endregion
+
+#pragma region Custom_Memory_Resources
+class Tracker : public std::pmr::memory_resource
+{
+private:
+	std::pmr::memory_resource* upstream;
+	std::string prefix{};
+
+public:
+	// we wrap the passed or default resource
+	explicit Tracker(std::pmr::memory_resource* us
+		= std::pmr::get_default_resource())
+		: upstream{ us } {
+	}
+
+	explicit Tracker(std::string p,
+		             std::pmr::memory_resource* us = std::pmr::get_default_resource())
+		: prefix{ std::move(p) }, upstream{ us } {
+	}
+
+private:
+	void* do_allocate(size_t bytes, size_t alignment) override {
+		std::cout << prefix << " allocate " << bytes << " Bytes\n";
+		void* ret = upstream->allocate(bytes, alignment);
+		return ret;
+	}
+
+	void do_deallocate(void* ptr, size_t bytes, size_t alignment) override {
+		std::cout << prefix << " deallocate " << bytes << " Bytes\n";
+		upstream->deallocate(ptr, bytes, alignment);
+	}
+
+	/* Determines if one polymorphic memory resource object can deallocate 
+	   memory allocated by another */
+	bool do_is_equal(const std::pmr::memory_resource& other) const noexcept
+		override {
+		if (this == &other) return true;
+		auto op = dynamic_cast<const Tracker*>(&other);
+		return op != nullptr && op->prefix == prefix
+			&& upstream->is_equal(other);
+	}
+};
+#pragma endregion
+
 
 int main() {
+	{
+		// track allocating chunks of memory (starting with 10k) without deallocating:
+		Tracker track1{ "keeppool:" };
+		std::pmr::monotonic_buffer_resource keeppool{ 10000, &track1 };
+		{
+			Tracker track2{ "  syncpool", &keeppool };
+			std::pmr::synchronized_pool_resource pool{ &track2 };
+
+			for (int j = 0; j < 100; ++j) {
+				std::pmr::vector<std::pmr::string> coll{ &pool };
+				coll.reserve(100);
+				for (int i = 0; i < 100; ++i) {
+					coll.emplace_back("just a non-SSO string");
+				}
+				if (j == 2) std::cout << "--- third iteration done\n";
+			}// deallocations are given back to the pool, but not deallocated
+			std::cout << "--- leave scope of pool\n";
+		} // so far nothing was deallocated
+		std::cout << "--- leave scope of keeppool\n";
+	} // deallocates all allocated memory
 	return 0;
 }
